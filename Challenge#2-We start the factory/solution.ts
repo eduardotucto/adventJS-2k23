649b1a51e90d63549203ae0d823913450a04fd27
Eduardo Tucto <tuctoeduardo@gmail.com>
2023-12-05 00:18:23 -0500

    challenge 2 completed

--- Challenge#2-We start the factory/solution.ts
+++ Challenge#2-We start the factory/solution.ts
@@ -1,4 +1,19 @@
 function manufacture(gifts: string[], materials: string) {
-  // Code here
-  return []
+  return gifts.filter(gift => {
+    return gift.split('').every(letter => {
+      return materials.includes(letter)
+    })
+  })
 }
+
+const gifts1 = ['tren', 'oso', 'pelota']
+const materials1 = 'tronesa'
+console.log(manufacture(gifts1, materials1)) // ["tren", "oso"]
+
+const gifts2 = ['juego', 'puzzle']
+const materials2 = 'jlepuz'
+console.log(manufacture(gifts2, materials2)) // ["puzzle"]
+
+const gifts3 = ['libro', 'ps5']
+const materials4 = 'psli'
+console.log(manufacture(gifts3, materials4)) // []
